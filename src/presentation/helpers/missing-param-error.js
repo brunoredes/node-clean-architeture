6c30ccf4454f5bcee90d47f9d992f679ae8ed0bd
brunoredes <bruno.donatelli@etec.sp.gov.br>
2021-02-02 16:52:02 -0300

    move spec files to test directory and creating helper directory to put httpresponse and missing-param-errors

---
+++ src/presentation/helpers/missing-param-error.js
@@ -0,0 +1,7 @@
+'use strict'
+module.exports = class MissingParamError extends Error {
+  constructor (paramName) {
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
+  }
+}
