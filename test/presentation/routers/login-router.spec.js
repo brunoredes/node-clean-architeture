1c1095cf99d43a7fe967d92365773e7c693269c9
brunoredes <bruno.donatelli@etec.sp.gov.br>
2021-02-03 19:09:09 -0300

    ensure Login-router return 500 if an invalid emailValidator is provided

--- test/presentation/routers/login-router.spec.js
+++ test/presentation/routers/login-router.spec.js
@@ -189,5 +189,33 @@ describe('Login Router', () => {
       expect(httpResponse.statusCode).toBe(400)
       expect(httpResponse.body).toEqual(new InvalidParamError('email'))
     })
+
+    test('Should return status code 500 if no EmailValidator is provided', async () => {
+      const authUseCaseSpy = makeAuthUseCaseSpy()
+      const sut = new LoginRouter(authUseCaseSpy)
+      const httpRequest = {
+        body: {
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        }
+      }
+      const httpResponse = await sut.route(httpRequest)
+      expect(httpResponse.statusCode).toBe(500)
+      expect(httpResponse.body).toEqual(new ServerError())
+    })
+
+    test('Should return status code 500 if no EmailValidator has no isValid method', async () => {
+      const authUseCaseSpy = makeAuthUseCaseSpy()
+      const sut = new LoginRouter(authUseCaseSpy, {})
+      const httpRequest = {
+        body: {
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        }
+      }
+      const httpResponse = await sut.route(httpRequest)
+      expect(httpResponse.statusCode).toBe(500)
+      expect(httpResponse.body).toEqual(new ServerError())
+    })
   })
 })
