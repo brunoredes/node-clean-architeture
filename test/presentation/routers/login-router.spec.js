dd28e8ac7e2e2056084ede9ea4f90e903bddefdf
brunoredes <bruno.donatelli@etec.sp.gov.br>
2021-02-03 11:38:58 -0300

    ensure loginrouter return a valid token when valid credentials are provided

--- test/presentation/routers/login-router.spec.js
+++ test/presentation/routers/login-router.spec.js
@@ -93,7 +93,7 @@ describe('Login Router', () => {
     })
 
     test('Should return status code 200 when valid credentials are provided', () => {
-      const { sut } = makeSut()
+      const { sut, authUseCaseSpy } = makeSut()
       const httpRequest = {
         body: {
           email: 'valid_email@mail.com',
@@ -102,6 +102,7 @@ describe('Login Router', () => {
       }
       const httpResponse = sut.route(httpRequest)
       expect(httpResponse.statusCode).toBe(200)
+      expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
     })
 
     test('Should return status code 500 if AuthUseCase has no auth method', () => {
