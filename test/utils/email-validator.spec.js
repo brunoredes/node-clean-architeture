afa101eaf0edec05219dc0951286507b64ebcb43
brunoredes <bruno.donatelli@etec.sp.gov.br>
2021-02-03 23:20:02 -0300

    ensure emailValidator calls validator with correct email

--- test/utils/email-validator.spec.js
+++ test/utils/email-validator.spec.js
@@ -16,10 +16,17 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('valid_email@mail.com')
     expect(isEmailValid).toBe(true)
   })
+
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
-    const isEmailValid = sut.isValid('invalid_email@')
+    const isEmailValid = sut.isValid('invalid_email@.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_mail@mail.com')
+    expect(validator.email).toBe('any_mail@mail.com')
   })
 })
